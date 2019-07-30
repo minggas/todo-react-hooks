536a0c27a1061238ca00286275f2d808b6cb007c
minggas <fasfora@gmail.com>
2019-07-30 11:56:20 -0300

    add button to edit todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,40 +1,55 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import TodoStyle from "../styles/TodoStyle";
 import ContentStyle from "../styles/ContentStyle";
-import DeleteBtnStyle from "../styles/DeleteBtnStyle";
+import TodoBtnStyle from "../styles/TodoBtnStyle";
 import CheckboxStyle from "../styles/CheckboxStyle";
 import { Context } from "./container/App";
 
-const Todo = ({ id, children, isChecked }) => {
+export default function Todo({ id, children, isChecked }) {
   const dispatch = useContext(Context);
-  const [editable, setEditable] = useState();
+  const [editable, setEditable] = useState(false);
+  const [current, setCurrent] = useState(children);
+
+  useEffect(() => {
+    setCurrent(children);
+  }, [children]);
 
   return (
-    <TodoStyle id={id} onDoubleClick={() => setEditable(!editable)}>
+    <TodoStyle id={id}>
       <CheckboxStyle
         type="checkbox"
         checked={isChecked}
         onChange={() => dispatch({ type: "completeTodo", payload: id })}
       />
       <ContentStyle
-        value={children}
+        value={current}
         readOnly={!editable}
-        onInput={e =>
-          dispatch({
-            type: "updateTodo",
-            payload: { id: id, text: e.target.value }
-          })
-        }
+        isChecked={isChecked}
+        onKeyPress={e => {
+          if (e.key === "Enter") {
+            setEditable(!editable);
+          }
+        }}
+        onChange={e => setCurrent(e.target.value)}
       />
-      <DeleteBtnStyle
+      <TodoBtnStyle
         onClick={() => {
-          dispatch({ type: "deleteTodo", payload: id });
+          if (editable) {
+            dispatch({
+              type: "updateTodo",
+              payload: { id: id, text: current }
+            });
+          }
+          setEditable(!editable);
         }}
       >
-        X
-      </DeleteBtnStyle>
+        {editable ? "Save" : "Edit"}
+      </TodoBtnStyle>
+      <TodoBtnStyle
+        onClick={() => dispatch({ type: "deleteTodo", payload: id })}
+      >
+        Delete
+      </TodoBtnStyle>
     </TodoStyle>
   );
-};
-
-export default Todo;
+}
