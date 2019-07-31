2c18555eb03d510d489f209f2b1d49816990a480
minggas <fasfora@gmail.com>
2019-07-31 01:57:52 -0300

    Updates

---
+++ precache-manifest.50ba505946e9c631f99efd13a3a34f9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e564c1420996c9fbfe1781a4f9e7dbd7",
+    "url": "/todo-react-hooks/index.html"
+  },
+  {
+    "revision": "d13aee904d20b731ca2e",
+    "url": "/todo-react-hooks/static/js/2.6479cc7e.chunk.js"
+  },
+  {
+    "revision": "ce5809381463190b51c4",
+    "url": "/todo-react-hooks/static/js/3.0ef3d510.chunk.js"
+  },
+  {
+    "revision": "d05aef0ea30ade01d757",
+    "url": "/todo-react-hooks/static/js/4.e0f8463a.chunk.js"
+  },
+  {
+    "revision": "e49675a1bc090980a869",
+    "url": "/todo-react-hooks/static/js/5.0a077039.chunk.js"
+  },
+  {
+    "revision": "32644686f68c7e94808e",
+    "url": "/todo-react-hooks/static/js/main.349f6898.chunk.js"
+  },
+  {
+    "revision": "21a5fc922731ac1e7bdd",
+    "url": "/todo-react-hooks/static/js/runtime~main.4ed2fb54.js"
+  }
+]);
