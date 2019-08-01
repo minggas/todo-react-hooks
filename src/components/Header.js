a58fafbc507dd556a8af05b7929000172334ae56
minggas <fasfora@gmail.com>
2019-08-01 20:56:44 -0300

    change initial header msg for date

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,19 @@ import React from "react";
 import { HeaderStyled } from "../styles/HeaderStyle";
 
 export default function Header(props) {
+  const locale = navigator.language || "en-US";
+  const today = new Date();
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    formatMatcher: "best fit"
+  };
+
   return (
     <HeaderStyled>
-      <h1>ToDo - List</h1>
+      <h1>{today.toLocaleDateString(locale, options)}</h1>
     </HeaderStyled>
   );
 }
