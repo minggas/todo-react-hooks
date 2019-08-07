0528bf292b35915cadec056cf42f54bfce79161b
minggas <fasfora@gmail.com>
2019-08-07 17:46:44 -0300

    feat add title and text to todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,43 @@
 import React, { useContext, useState, useEffect } from "react";
 import TodoStyle from "../styles/TodoStyle";
-import { ContentStyle, ContentEditStyle } from "../styles/ContentStyle";
+import { MarkdownPreview, MarkdownInput } from "../components/Markdown";
 import TodoBtnStyle from "../styles/TodoBtnStyle";
 import CheckboxStyle from "../styles/CheckboxStyle";
 import { Context } from "./container/App";
+import marked from "marked";
 
-export default function Todo({ id, children, isChecked }) {
+export default function Todo({ id, text, title, isChecked }) {
   const dispatch = useContext(Context);
   const [editable, setEditable] = useState(false);
-  const [current, setCurrent] = useState(children);
+  const [actual_title, setTitle] = useState(title);
+  const [actual_text, setText] = useState(text);
+  const renderer = new marked.Renderer();
+  renderer.link = function(href, title, text) {
+    return `<a target="_blank" href="${href}">${text}</a>`;
+  };
+
+  marked.setOptions({
+    breaks: true
+  });
+  const mark = {
+    __html: marked(actual_text, { renderer: renderer })
+  };
 
   useEffect(() => {
-    setCurrent(children);
-  }, [children]);
+    setText(text);
+  }, [text]);
+
+  useEffect(() => {
+    setTitle(title);
+  }, [title]);
 
   return (
-    <TodoStyle id={id} editable={editable} onClick={() => setEditable(true)}>
+    <TodoStyle
+      id={id}
+      isChecked={isChecked}
+      editable={editable}
+      onClick={() => setEditable(true)}
+    >
       {editable ? (
         <>
           <CheckboxStyle
@@ -23,16 +45,23 @@ export default function Todo({ id, children, isChecked }) {
             checked={isChecked}
             onChange={() => dispatch({ type: "completeTodo", payload: id })}
           />
-          <ContentEditStyle
-            value={current}
-            onChange={e => setCurrent(e.target.value)}
-          />
+          <div>
+            <input
+              type="text"
+              value={actual_title}
+              onChange={e => setTitle(e.target.value)}
+            />
+            <MarkdownInput
+              value={actual_text}
+              onChange={e => setText(e.target.value)}
+            />
+          </div>
           <TodoBtnStyle
             onClick={() => {
               if (editable) {
                 dispatch({
                   type: "updateTodo",
-                  payload: { id: id, text: current }
+                  payload: { id: id, title: actual_title, text: actual_text }
                 });
               }
               setEditable(false);
@@ -47,7 +76,10 @@ export default function Todo({ id, children, isChecked }) {
           </TodoBtnStyle>
         </>
       ) : (
-        <ContentStyle isChecked={isChecked}>{current}</ContentStyle>
+        <div>
+          <h1>{title}</h1>
+          <MarkdownPreview value={mark} />
+        </div>
       )}
     </TodoStyle>
   );
